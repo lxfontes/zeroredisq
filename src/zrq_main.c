fc2fd29e3ae71ab577fefcdc851692d483618e9b
Lucas Fontes <lxfontes@gmail.com>
2012-03-14 16:35:17 -0400

    moving to bdb

--- src/zrq_main.c
+++ src/zrq_main.c
@@ -2,24 +2,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <jansson.h>
-#include "uthash.h"
-#include <jemalloc/jemalloc.h>
+#include <db.h>
 
-typedef struct db_entry {
-	char *key;
-	json_t *val;
-	UT_hash_handle hh;
-} db_entry_t;
+
+typedef struct zrq_db {
+	DB *dbp;
+} zrq_db_t;
+
+
+int zrq_set_key(DB *dbp, const char *skey, size_t slen, const char *sval, size_t vlen)
+{
+	DBT key, val;
+	int ret;
+
+	memset(&key, 0, sizeof(DBT));
+	memset(&val, 0, sizeof(DBT));
+
+	key.data = (void *)skey;
+	key.size = slen + 1;
+
+	val.data = (void *)sval;
+	val.size = vlen + 1;
+
+	ret = dbp->put(dbp, NULL, &key, &val, 0);
+
+	return ret;
+}
+
+int zrq_get_key(DB *dbp, const char *str, size_t len, DBT *retdbt)
+{
+	DBT key,val;
+	int ret;
+
+	memset(&key, 0, sizeof(DBT));
+	memset(&val, 0, sizeof(DBT));
+
+	key.data = (void *)str;
+	key.size = len + 1;
+
+	ret = dbp->get(dbp, NULL, &key, &val, 0);
+
+	if (ret == DB_NOTFOUND) {
+		return -1;
+	}
+
+	memcpy(retdbt, &val, sizeof(DBT));
+	return 0;
+}
 
 
 int zrq_set(zrq_zmq_t *z, json_t *jreq, json_t *jresp)
 {
 	json_t *jop;
 	json_t *jv;
-	json_t *value;
 	const char *key;
-	db_entry_t *db = ZRQ_ZMQ_DATA(z);
-	db_entry_t *entry = NULL;
+	zrq_db_t *db = ZRQ_ZMQ_DATA(z);
+	int ret;
+	char *sval;
+	int vlen;
 
 	jop = json_object_get(jreq, "SET");
 	key = json_string_value(jop);
@@ -27,27 +67,17 @@ int zrq_set(zrq_zmq_t *z, json_t *jreq, json_t *jresp)
 
 	if (jv == NULL) return 1;
 
-	value = json_copy(jv);
+	sval = json_dumps(jv, JSON_COMPACT | JSON_ENCODE_ANY);
+	vlen = strlen(sval);
 
-	HASH_FIND_STR(db, key, entry);
+	ret = zrq_set_key(db->dbp, key, strlen(key), sval, vlen);
+	free(sval);
 
-	if (entry) {
-		json_decref(entry->val);
-		entry->val = value;
+	if (ret != 0) {
+		json_object_set_new(jresp, "result", json_string("error"));
 	} else {
-		entry = malloc(sizeof(*entry));
-		if (entry == NULL) {
-			json_decref(entry->val);
-			json_object_set_new(jresp, "result", json_string("error"));
-			return 0;
-		}
-		entry->key = strdup(key);
-		entry->val = value;
-		HASH_ADD_KEYPTR(hh, db, entry->key, strlen(entry->key), entry);
-		ZRQ_ZMQ_DATA(z) = db;
+		json_object_set_new(jresp, "result", json_string("ok"));
 	}
-
-	json_object_set_new(jresp, "result", json_string("ok"));
 
 	return 0;
 }
@@ -56,9 +86,11 @@ int zrq_get(zrq_zmq_t *z, json_t *jreq, json_t *jresp)
 {
 	json_t *jop;
 	json_t *el;
+	json_error_t err;
 	const char *key;
-	db_entry_t *db = ZRQ_ZMQ_DATA(z);
-	db_entry_t *entry = NULL;
+	int ret;
+	zrq_db_t *db = ZRQ_ZMQ_DATA(z);
+	DBT val;
 	jop = json_object_get(jreq, "GET");
 
 	if (json_is_array(jop)) {
@@ -70,21 +102,31 @@ int zrq_get(zrq_zmq_t *z, json_t *jreq, json_t *jresp)
 			key = json_string_value(el);
 
 
-			HASH_FIND_STR(db, key, entry);
+			ret = zrq_get_key(db->dbp, key, strlen(key), &val);
 
-			if (entry) {
-				json_object_set(jresp, key, entry->val);
-			}else{
+			if (ret) {
+				//doesnt exist
 				json_object_set_new(jresp, key, json_null());
+			}else{
+				char *v = val.data;
+				json_t *tj = json_loads(v, JSON_DECODE_ANY, &err);
+				json_object_set(jresp, key, tj);
+				json_decref(tj);
 			}
 		}
 	} else if(json_is_string(jop)) {
 		key = json_string_value(jop);
-		HASH_FIND_STR(db, key, entry);
-		if (entry) {
-			json_object_set(jresp, key, entry->val);
-		}else{
+
+		ret = zrq_get_key(db->dbp, key, strlen(key), &val);
+
+		if (ret) {
+			//doesnt exist
 			json_object_set_new(jresp, key, json_null());
+		}else{
+			char *v = val.data;
+			json_t *tj = json_loads(v, JSON_DECODE_ANY, &err);
+			json_object_set(jresp, key, tj);
+			json_decref(tj);
 		}
 	} else {
 		return 1;
@@ -127,6 +169,8 @@ int zrq_callback(zrq_zmq_t *z, char *data_in, size_t len_in, char **data_out, si
 int main(int argc, char **argv)
 {
 	zrq_zmq_t *zmq;
+	zrq_db_t db;
+	u_int32_t flags;
 	char *default_endpoint = "tcp://*:5000";
 	char *endpoint;
 
@@ -135,9 +179,14 @@ int main(int argc, char **argv)
 	else
 		endpoint = default_endpoint;
 
+	db_create(&db.dbp, NULL, 0);
+
+	flags = DB_CREATE | DB_PRIVATE;
+	db.dbp->open(db.dbp, NULL, NULL, NULL, DB_BTREE, flags, 0);
+
 	zmq = zrq_zmq_create(endpoint, zrq_callback);
 
-	ZRQ_ZMQ_DATA(zmq) = NULL;
+	ZRQ_ZMQ_DATA(zmq) = &db;
 
 	zrq_zmq_run(zmq);
 	return 0;
